765174ebe419fad4468979cc4effc501795403a1
Cha673 <spillebout.charlotte@gmail.com>
2025-10-06 15:30:27 +0200

    Create CRUD account - Etape 1

--- app/Persistence/Interfaces/AccountRepositoryInterface.php
+++ app/Persistence/Interfaces/AccountRepositoryInterface.php
@@ -9,5 +9,6 @@ interface AccountRepositoryInterface
     public function getAll(): array;
     public function create(AccountEntity $account): AccountEntity;
     public function delete($id): bool;
+    //trouver le compte correspond à un userId afin de le supprimer
     public function findByUserId(int $userId): ?AccountEntity;
 }
